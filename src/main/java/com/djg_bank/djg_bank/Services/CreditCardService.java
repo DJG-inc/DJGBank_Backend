e0edccc0b65138630435c30c6ea1108113c4981a
Dillan <dillansdev@gmail.com>
2023-09-12 22:04:10 -0500

    The resource bank class was added that contains the main functions of the bank

--- src/main/java/com/djg_bank/djg_bank/Services/CreditCardService.java
+++ src/main/java/com/djg_bank/djg_bank/Services/CreditCardService.java
@@ -7,25 +7,26 @@ import com.djg_bank.djg_bank.Models.UserModel;
 import com.djg_bank.djg_bank.Repositories.ICreditCardRepository;
 import com.djg_bank.djg_bank.Repositories.IUserRepository;
 import com.djg_bank.djg_bank.Utils.ErrorResponse;
+import com.djg_bank.djg_bank.Utils.ResourcesBank;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
-import java.util.Random;
 
 @Service
 public class CreditCardService {
     private final ICreditCardRepository creditCardRepository;
     private final CreditCardMapper creditCardMapper;
     private final IUserRepository userRepository;
+    private final ResourcesBank resourcesBank;
 
-    public CreditCardService(ICreditCardRepository creditCardRepository, CreditCardMapper creditCardMapper, IUserRepository userRepository) {
+    public CreditCardService(ICreditCardRepository creditCardRepository, CreditCardMapper creditCardMapper, IUserRepository userRepository, ResourcesBank resourcesBank) {
         this.creditCardRepository = creditCardRepository;
         this.creditCardMapper = creditCardMapper;
         this.userRepository = userRepository;
+        this.resourcesBank = resourcesBank;
     }
 
     public ResponseEntity<?> createCreditCard(Long id, String cardType) {
@@ -44,7 +45,7 @@ public class CreditCardService {
             Date dateOfBirth = dateFormat.parse(dateOfBirthString);
 
             // Calcular la edad del usuario a partir de la fecha de nacimiento
-            int age = calcularEdad(dateOfBirth);
+            int age = this.resourcesBank.calculateAge(dateOfBirth);
 
             // Verificar que el usuario tenga al menos 18 años
             if (age < 18) {
@@ -52,12 +53,12 @@ public class CreditCardService {
             }
 
             // Validar el tipo de tarjeta (Visa o MasterCard)
-            if (!isValidCardType(cardType)) {
+            if (this.resourcesBank.isValidCardType(cardType)) {
                 return new ResponseEntity<>(new ErrorResponse("Tipo de tarjeta no válido"), HttpStatus.BAD_REQUEST);
             }
 
             // Generar un número de tarjeta válido según el tipo seleccionado
-            String cardNumber = generateValidCardNumber(cardType);
+            String cardNumber = this.resourcesBank.generateValidCardNumber(cardType);
 
             // Crear la tarjeta de crédito con valores automáticos
             CreditCardModel creditCardModel = new CreditCardModel();
@@ -65,11 +66,11 @@ public class CreditCardService {
             creditCardModel.setCard_type(cardType);
 
             // Generar y establecer la fecha de vencimiento (expiry_date) automáticamente
-            Date expiryDate = generateExpiryDate();
+            Date expiryDate = this.resourcesBank.generateExpiryDate();
             creditCardModel.setExpiry_date(expiryDate);
 
             // Generar y establecer un CVV aleatorio de tres dígitos
-            String cvv = generateRandomCVV();
+            String cvv = this.resourcesBank.generateRandomCVV();
             creditCardModel.setCvv(cvv);
 
             // Establecer la fecha de emisión (date_issued) como la fecha actual
@@ -97,80 +98,4 @@ public class CreditCardService {
         }
     }
 
-    private boolean isValidCardType(String cardType) {
-        // Validar que el tipo de tarjeta sea "Visa" o "MasterCard"
-        if (cardType == null) {
-            return false;
-        }
-        else return cardType.equalsIgnoreCase("Visa") || cardType.equalsIgnoreCase("MasterCard");
-    }
-
-    private String generateValidCardNumber(String cardType) {
-        // Generar un número de tarjeta válido según el tipo seleccionado (Visa o MasterCard)
-        Random random = new Random();
-        StringBuilder cardNumber = new StringBuilder();
-        cardNumber.append(cardType.equalsIgnoreCase("Visa") ? "4" : "5"); // Prefijo para Visa o MasterCard
-
-        for (int i = 0; i < 15; i++) {
-            cardNumber.append(random.nextInt(10)); // Generar los 15 dígitos restantes
-        }
-
-        // Aplicar el algoritmo de Luhn para generar el dígito de verificación
-        cardNumber.append(generateLuhnDigit(cardNumber.toString()));
-
-        return cardNumber.toString();
-    }
-
-    private int generateLuhnDigit(String cardNumber) {
-        // Implementar el algoritmo de Luhn para generar el dígito de verificación
-        int sum = 0;
-        boolean doubleDigit = false;
-
-        for (int i = cardNumber.length() - 1; i >= 0; i--) {
-            int digit = Character.getNumericValue(cardNumber.charAt(i));
-
-            if (doubleDigit) {
-                digit *= 2;
-                if (digit > 9) {
-                    digit -= 9;
-                }
-            }
-
-            sum += digit;
-            doubleDigit = !doubleDigit;
-        }
-
-        return (sum * 9) % 10;
-    }
-
-    private Date generateExpiryDate() {
-        // Generar la fecha de vencimiento como 5 años a partir de la fecha actual
-        Date currentDate = new Date();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(currentDate);
-        calendar.add(Calendar.YEAR, 5);
-        return calendar.getTime();
-    }
-
-    private String generateRandomCVV() {
-        // Generar un CVV aleatorio de tres dígitos
-        Random random = new Random();
-        int cvv = random.nextInt(1000); // Número aleatorio de 0 a 999
-        return String.format("%03d", cvv); // Formatear como cadena de tres dígitos
-    }
-
-    private int calcularEdad(Date dateOfBirth) {
-        Calendar dob = Calendar.getInstance();
-        dob.setTime(dateOfBirth);
-
-        Calendar currentDate = Calendar.getInstance();
-
-        int age = currentDate.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
-
-        if (currentDate.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
-            age--;
-        }
-
-        return age;
-    }
 }
---
+++ src/main/java/com/djg_bank/djg_bank/Utils/ResourcesBank.java
@@ -0,0 +1,87 @@
+package com.djg_bank.djg_bank.Utils;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
+
+@Component
+public class ResourcesBank {
+    public boolean isValidCardType(String cardType) {
+        // Validar que el tipo de tarjeta sea "Visa" o "MasterCard"
+        if (cardType == null) {
+            return true;
+        }
+        else return !cardType.equalsIgnoreCase("Visa") && !cardType.equalsIgnoreCase("MasterCard");
+    }
+
+    public String generateValidCardNumber(String cardType) {
+        // Generar un número de tarjeta válido según el tipo seleccionado (Visa o MasterCard)
+        Random random = new Random();
+        StringBuilder cardNumber = new StringBuilder();
+        cardNumber.append(cardType.equalsIgnoreCase("Visa") ? "4" : "5"); // Prefijo para Visa o MasterCard
+
+        for (int i = 0; i < 15; i++) {
+            cardNumber.append(random.nextInt(10)); // Generar los 15 dígitos restantes
+        }
+
+        // Aplicar el algoritmo de Luhn para generar el dígito de verificación
+        cardNumber.append(generateLuhnDigit(cardNumber.toString()));
+
+        return cardNumber.toString();
+    }
+
+    public int generateLuhnDigit(String cardNumber) {
+        // Implementar el algoritmo de Luhn para generar el dígito de verificación
+        int sum = 0;
+        boolean doubleDigit = false;
+
+        for (int i = cardNumber.length() - 1; i >= 0; i--) {
+            int digit = Character.getNumericValue(cardNumber.charAt(i));
+
+            if (doubleDigit) {
+                digit *= 2;
+                if (digit > 9) {
+                    digit -= 9;
+                }
+            }
+
+            sum += digit;
+            doubleDigit = !doubleDigit;
+        }
+
+        return (sum * 9) % 10;
+    }
+
+    public Date generateExpiryDate() {
+        // Generar la fecha de vencimiento como 5 años a partir de la fecha actual
+        Date currentDate = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(currentDate);
+        calendar.add(Calendar.YEAR, 5);
+        return calendar.getTime();
+    }
+
+    public String generateRandomCVV() {
+        // Generar un CVV aleatorio de tres dígitos
+        Random random = new Random();
+        int cvv = random.nextInt(1000); // Número aleatorio de 0 a 999
+        return String.format("%03d", cvv); // Formatear como cadena de tres dígitos
+    }
+
+    public int calculateAge(Date dateOfBirth) {
+        Calendar dob = Calendar.getInstance();
+        dob.setTime(dateOfBirth);
+
+        Calendar currentDate = Calendar.getInstance();
+
+        int age = currentDate.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if (currentDate.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+
+        return age;
+    }
+}
